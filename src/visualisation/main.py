74c58282ef9938064586627956b912467b2451c3
Collin Aldaibis <caldaibis@gmail.com>
2022-01-15 19:04:30 +0100

    visualise one algo performance over time

--- src/visualisation/main.py
+++ src/visualisation/main.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 
 def run():
-    pass
+    df = pd.read_csv('progress.csv')
+
+    sns.set_theme(style="darkgrid")
+    sns.lineplot(x="time_total_s", y="surrogate_reward_mean", data=df)
+    plt.show()
 
 
 if __name__ == '__main__':
